bd0c8e32fb331a6a97fc7a487a3ac517ed66dff3
dulldull132 <90049067+dulldull132@users.noreply.github.com>
2021-09-09 23:03:25 +0900

    Initial commit

---
+++ grid.py
@@ -0,0 +1,29 @@
+import turtle
+count = 6
+while (count > 0):
+	turtle.forward(500)
+	turtle.left(90)
+	turtle.penup()
+	turtle.forward(100)
+	turtle.left(90)
+	turtle.forward(500)
+	turtle.right(180)
+	turtle.pendown()
+	count = count -1
+
+	
+turtle.right(90)
+turtle.penup()
+turtle.forward(100)
+turtle.pendown()
+count=6
+while (count > 0):
+	turtle.forward(500)
+	turtle.left(90)
+	turtle.penup()
+	turtle.forward(100)
+	turtle.left(90)
+	turtle.forward(500)
+	turtle.right(180)
+	turtle.pendown()
+	count = count -1
